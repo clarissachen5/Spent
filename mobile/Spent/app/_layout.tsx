6835fe732a9086cd7315e647fe34096ac5152ee5
clarissachen5 <clarissachen5@gmail.com>
2026-03-26 16:35:29 -0400

    figuring out how to run

--- mobile/Spent/app/_layout.tsx
+++ mobile/Spent/app/_layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-          <Stack.Screen name="signup" options={{ headerShown: false }} />
           <Stack.Screen name="checkin" options={{ title: 'Check In' }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         </Stack>
