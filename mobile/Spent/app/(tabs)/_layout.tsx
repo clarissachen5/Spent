a245650bfb536a01ceb780ad5c737dd85b695feb
clarissachen5 <clarissachen5@gmail.com>
2026-03-29 11:39:01 -0400

    app .json edit

--- mobile/Spent/app/(tabs)/_layout.tsx
+++ mobile/Spent/app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ export default function TabLayout() {
 
   return (
     <Tabs
-      initialRouteName="signup"
+      initialRouteName="home"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
