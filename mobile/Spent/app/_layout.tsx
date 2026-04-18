e55744673227d4ea637dd097382b25a2dd199a35
ishaborkar <ishab@bu.edu>
2026-04-18 13:27:40 -0400

    Onboarding pages
    First draft

--- mobile/Spent/app/_layout.tsx
+++ mobile/Spent/app/_layout.tsx
@@ -5,7 +5,6 @@ import 'react-native-reanimated';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import * as firebase from '../src/config/firebase';
 import { AuthProvider } from '../context/AuthContext';
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -14,17 +13,12 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  console.log('firebase instance:', firebase)
-
-  //  write some console log to test firebase is working 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
     <AuthProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-          <Stack.Screen name="signup" options={{ headerShown: false }} />
           <Stack.Screen name="checkin" options={{ title: 'Check In' }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         </Stack>
